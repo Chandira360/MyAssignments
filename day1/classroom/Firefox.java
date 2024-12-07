8dcfc1eb6c75c3e2542350ae647730b5c07416d5
Chandira360 <tschandiralekha@gmail.com>
2024-12-07 17:50:13 +0530

    Add files via upload
    Day 1

---
+++ day1/classroom/Firefox.java
@@ -0,0 +1,23 @@
+package day1.classroom;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion=100.2f;
+		String browserName="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo='f';
+		System.out.println("The Details are:");
+		System.out.println("The Browser Version is:"+browserVersion+";\n "
+				+ "The Browser Name is:"+browserName+";\n"
+						+ "Is the Browser Visible:"+isVisible+";\n"
+								+ "The Browser Released At:"+releaseYear+";\n"
+										+ "The Browser Logo:"+browserLogo+";"
+						
+				);
+
+	}
+
+}
---
+++ day1/datatype/Chrome.java
@@ -0,0 +1,23 @@
+package day1.datatype;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version=91.0f;
+		String developer="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='c';
+		System.out.println("The Chrome Details are:");
+		System.out.println("The Version is:"+version+";\n"
+				+ "The Developer is:"+developer+";\n"
+						+ "IsBeta Value:"+isBeta+";\n"
+								+ "The Browser Released At:"+releaseYear+";\n"
+										+ "The Short cut key:"+shortcutKey+";"
+						
+				);
+
+	}
+
+}
